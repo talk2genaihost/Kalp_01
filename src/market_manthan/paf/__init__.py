c05433fefb7d0cdf80ccf7fa143701f5b683e6e4
talk2genaihost <gaurg@hotmail.com>
2026-09-04 01:51:50 +0530

    chore(market-manthan): initialize PAF package

---
+++ src/market_manthan/paf/__init__.py
@@ -0,0 +1,11 @@
+from .contracts import MarketRecord, ProviderAdapter, ProviderHealth, ProviderRegistry, QualityMetadata
+from .reference_adapter import ReferenceProviderAdapter
+
+__all__ = [
+    "MarketRecord",
+    "ProviderAdapter",
+    "ProviderHealth",
+    "ProviderRegistry",
+    "QualityMetadata",
+    "ReferenceProviderAdapter",
+]
